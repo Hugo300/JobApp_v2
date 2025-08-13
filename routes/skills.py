74c96325959c474c0abac343ac5c8648ad1086d5
Hugo Ferreira <hugo.ferreira3000@gmail.com>
2025-08-13 18:36:58 +0100

    added routes to view, delete, and blacklist skills and categories

--- routes/skills.py
+++ routes/skills.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, request, jsonify, render_template
-from services.skill_service import SkillService
-
-from utils.json import make_serializable
+from flask import Blueprint, redirect, request, jsonify, render_template, url_for
+from services import SkillService, CategoryService
+from models import db
 
 skills_bp = Blueprint('skills', __name__)
 
@@ -44,6 +43,118 @@ def extract_skills():
         "extracted_skills": extracted_skills,
         "categorized_skills": categorized_skills
     }), 200
+
+
+
+@skills_bp.route('/manage')
+def skills_manage():
+
+    skill_service = SkillService()
+    skills = skill_service.get_all_skills_and_category()
+
+    category_service = CategoryService()
+    categories = category_service.get_all_categories()
+
+    blacklist = skill_service.get_blacklist_skills()
+
+    return render_template(
+        'admin/skills.html',
+        categories=categories,
+        skills=skills,
+        blacklisted_words=blacklist
+    )
+
+@skills_bp.route('/create_skill', methods=['POST'])
+def skill_create():
+    try:
+        skill_service = SkillService()
+
+        name = request.form.get('name')
+        category = request.form.get('category_id')
+
+        result = skill_service.create_skill(name=name, category=category, is_blacklisted=False)
+        print(result)
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+
+    return redirect(url_for('skills.skills_manage'))
+
+
+@skills_bp.route('/<int:skill_id>/delete', methods=['POST'])
+def skill_delete(skill_id):
+    try:
+        skill_service = SkillService()
+        skill_service.delete_skill(skill_id=skill_id)
+    except Exception as e:
+        db.session.rollback()
+
+    return redirect(url_for('skills.skills_manage'))
+
+
+
+@skills_bp.route('/create_blacklist', methods=['POST'])
+def blacklist_create():
+    try:
+        skill_service = SkillService()
+
+        name = request.form.get('name')
+        category = request.form.get('category_id')
+
+        result = skill_service.create_skill(name=name, category=category, is_blacklisted=True)
+        print(result)
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+
+    return redirect(url_for('skills.skills_manage'))
+
+@skills_bp.route('/<int:skill_id>/add_blacklist', methods=['POST'])
+def skill_add_blacklist(skill_id):
+    try:
+        skill_service = SkillService()
+        skill_service.set_blacklist(skill_id=skill_id, value=True)
+    except Exception as e:
+        db.session.rollback()
+
+    return redirect(url_for('skills.skills_manage'))
+
+@skills_bp.route('/<int:skill_id>/remove_blacklist', methods=['POST'])
+def skill_remove_blacklist(skill_id):
+    try:
+        skill_service = SkillService()
+        skill_service.set_blacklist(skill_id=skill_id, value=False)
+    except Exception as e:
+        db.session.rollback()
+
+    return redirect(url_for('skills.skills_manage'))
+
+
+@skills_bp.route('/create_category', methods=['POST'])
+def category_create():
+    try:
+        service = CategoryService()
+
+        name = request.form.get('name')
+        description = request.form.get('description')
+
+        result = service.create_category(name=name, description=description)
+        print(result)
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+
+    return redirect(url_for('skills.skills_manage'))
+
+@skills_bp.route('/<int:category_id>/delete', methods=['POST'])
+def category_delete(category_id):
+    print('hello')
+    print(category_id)
+    return redirect(url_for('skills.skills_manage'))
+
+
+
+
 
 
 @skills_bp.route('/test', methods=['GET'])
