f9ea5b6cc83fc25d41b490bf8ad2989a6f0d85ce
Hugo Ferreira <hugo.ferreira3000@gmail.com>
2025-08-22 16:55:05 +0100

    Added skill variant model. This allows us to define alternative words that refer the same skill

--- models/skill.py
+++ models/skill.py
@@ -13,7 +13,8 @@ class Skill(db.Model):
     skill_jobs = relationship('JobSkill', backref='skills', lazy=True, cascade='all, delete-orphan')
     skill_user = relationship('UserSkill', backref='skills', lazy=True, cascade='all, delete-orphan')
 
-    skill_category = relationship('SkillCategory', backref='skills', lazy=True)
+    category = relationship('SkillCategory', back_populates='skills', lazy=True)
+    variants = relationship('SkillVariant', back_populates='skill', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"<Skill(name={self.name}, category_id={self.category_id})>"
@@ -26,9 +27,23 @@ class SkillCategory(db.Model):
     name = db.Column(db.String(255), nullable=False, unique=True)
     description = db.Column(db.String(500), nullable=True)
 
+    skills = relationship("Skill", back_populates="category")
+
     def __init__(self, name, description=None):
         self.name = name
         self.description = description
 
     def __repr__(self):
         return f"<SkillCategory(name={self.name})>"
+
+
+class SkillVariant(db.Model):
+    __tablename__ = 'skill_variants'
+    id = db.Column(db.Integer, primary_key=True)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), nullable=False)
+    variant_name = db.Column(db.String(255), nullable=False)
+
+    skill = relationship('Skill', back_populates='variants')
+    
+    def __repr__(self):
+        return f'<SkillVariant {self.variant_name} -> {self.skill.name}>'
